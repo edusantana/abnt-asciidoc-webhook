95b45278cfaec1f131021c2455c076ea34afd6c3
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2015-06-19 21:04:51 -0300

    dblatex.conf

--- server.rb
+++ server.rb
@@ -2,16 +2,16 @@ require 'sinatra'
 require 'json'
 require 'git'
 
+DBLATEX_CONF_FILE = 'dblatex.conf'
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   #puts "I got some JSON: #{push.inspect}"
 
-  owner = push['repository']['owner']['login']
-  repo_name = push['repository']['name']
+  owner = push['repository']['owner']['login'] #edusantana
+  repo_name = push['repository']['name'] #playground-asciidoc
   full_name = push['repository']['full_name'] # edusantana/playground-asciidoc
-
   git_url = push['repository']['git_url']
-  full_name = push['repository']['full_name']
   working_dir = full_name
 
   begin
@@ -25,7 +25,11 @@ post '/payload' do
   Dir.chdir("#{working_dir}/livro") do
     system %Q(asciidoctor livro.asc)
     system %Q(asciidoctor -d book -b docbook5 livro.asc)
-    system %Q(dblatex -P latex.encoding=utf8 livro.xml)
+
+    # If file DBLATEX_CONF_FILE exists it will be used as configuration file
+    # http://dblatex.sourceforge.net/doc/manual/sec-specs.html
+    dblatex_conf = File.exists?(DBLATEX_CONF_FILE) ? "-S #{DBLATEX_CONF_FILE}" : ""
+    system %Q(dblatex #{dblatex_conf} livro.xml)
   end
 
 end
